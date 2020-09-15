b14e21e56eaad1f703b0f9b2643394901eea7648
Durbich <64540953+Durbich@users.noreply.github.com>
2020-09-15 23:34:52 +0300

    Add files via upload

--- API.py
+++ API.py
@@ -2,31 +2,32 @@ from configparser import ConfigParser
 import requests
 import json
 import identity
-"""Этот модуль делает запросы к ВК и больше ничего"""
+# Этот модуль делает запросы к ВК и больше ничего
 
 
 class API:
     
-    def __init__(self):
-        """Создание переменных, нужных для подключения бота к серверам ВК"""
-        
+    def __init__(self):   
         cfg = ConfigParser()
         cfg.read("config.ini")
         self.HOME = cfg["API"]["HOME"]
         self.VER = cfg["API"]["VER"]
-        self.WAIT = cfg["API"]["WAIT"]
+        self.WAIT = int(cfg["API"]["WAIT"])
         self.GROUP_ID = cfg["GROUP"]["ID"]
         self.TOKEN = cfg["GROUP"]["TOKEN"]
         self.TAIL = f"&v={self.VER}&access_token={self.TOKEN}"
+        self.lp_err = 0
 
     def get_long_poll(self, reason):
-        """Подключение к серверу обновлений"""
-    
         while True:
             try:
-                lp = requests.get(f"{self.HOME}groups.getLongPollServer?group_id={self.GROUP_ID}{self.TAIL}").json()["response"]
+                lp = requests.get(f"{self.HOME}groups.getLongPollServer?group_id={self.GROUP_ID}{self.TAIL}", timeout = self.WAIT).json()["response"]
             except TimeoutError:
-                print('Get LP timeout')
+                print('-- catch LP timeout --')
+            except ConnectionError:
+                print('-- catch connect error --')
+            except ConnectionAbortedError:
+                print('-- connection aborted --')
             else:
                 break # Ток после успешного подключения выйдет из цикла (сделано против Таймаута или если сеть присела)
         if reason == 'full':
@@ -37,11 +38,9 @@ class API:
             self.lp_ts = lp["ts"]
     
     def listen_LP(self):
-        """Чтение из сервера обновлений"""
-    
         question = f"{self.lp_server}?act=a_check&key={self.lp_key}&ts={self.lp_ts}&wait={self.WAIT}"
-        answer = requests.get(question).content
-        lp_answer = json.loads(answer)
+        lp_answer = requests.get(question, timeout = self.WAIT + 2).json()
+        # lp_answer = json.loads(answer)
         try:
             self.lp_ts = lp_answer["ts"]
             return ('good', lp_answer["updates"])
@@ -57,13 +56,17 @@ class API:
             except TimeoutError:
                 print('-- Listen LP timeout --')
                 return ('err', 'relogin')
+            except ConnectionError:
+                print('-- Listen LP connect error --')
+                return ('err', 'relogin')
+            except ConnectionAbortedError:
+                print('-- Listen LP connection aborted --')
+                return ('err', 'relogin')
     
     def exe(self, order):
-        """Отдаёт команды на сервер ВК"""
-    
         full_order = self.HOME + order + self.TAIL
         try:
-            answer = requests.get(full_order).content.decode('utf-8')
+            answer = requests.get(full_order, timeout = self.WAIT).content.decode('utf-8')
         except:
             print ('ERR CONNECT')
         else:
